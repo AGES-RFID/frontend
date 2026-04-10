cbd164f32602f1e928e40ac1fbdbdcfbc4996df8
Bruno Panizzi de Quadros <brunopanizziq@gmail.com>
2026-04-09 21:47:18 -0300

    fix: improve environment variable loading, avoiding errors and using sensible defaults

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,11 +1,10 @@
 import ky from "ky";
-
-const BASE_URL = process.env.PUBLIC_API_URL ?? "http://localhost:5000";
+import { env } from "@/config/env";
 
 export type ApiClient = typeof ky;
 
 export const api = ky.extend({
-  prefixUrl: `${BASE_URL}/api`,
+  prefixUrl: `${env.API_URL}/api`,
   hooks: {
     beforeRequest: [],
   },
