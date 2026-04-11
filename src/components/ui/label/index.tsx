5dc9d3d3c8cfb71a1a491d1b24a8055354fba8d6
Bruno Panizzi de Quadros <brunopanizziq@gmail.com>
2026-04-11 10:26:52 -0300

    fix: simplify styles

--- src/components/ui/label/index.tsx
+++ src/components/ui/label/index.tsx
@@ -1,53 +1,28 @@
-import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/utils/cn";
 
-const labelContainerStyles = cva(
-  [
-    "inline-flex flex-col gap-1",
-    "select-none cursor-pointer",
-    "rounded",
-    "transition-colors duration-150",
-  ],
-  {
-    variants: {},
-    defaultVariants: {},
-  },
-);
-
-const labelTextStyles = cva(
-  ["text-[14px] leading-[20px]", "font-bold", "text-[#000000]"],
-  {
-    variants: {},
-    defaultVariants: {},
-  },
-);
-
-const valueTextStyles = cva(
-  [
-    "text-[14px] leading-[20px]",
-    "font-normal",
-    "text-[#020617]",
-    "flex items-center gap-2",
-    "before:content-['•'] before:text-[#020617] before:text-[16px] before:leading-[20px]",
-  ],
-  {
-    variants: {},
-    defaultVariants: {},
-  },
-);
-
-type LabelProps = React.HTMLAttributes<HTMLDivElement> &
-  VariantProps<typeof labelContainerStyles> & {
-    label: string;
-    value: string;
-  };
+type LabelProps = React.HTMLAttributes<HTMLDivElement> & {
+  label: string;
+  value: string;
+};
 
 export function Label({ label, value, className, ...props }: LabelProps) {
   return (
-    <div className={cn(labelContainerStyles(), "group", className)} {...props}>
+    <div
+      className={cn(
+        "inline-flex flex-col gap-1",
+        "cursor-pointer select-none",
+        "rounded",
+        "transition-colors duration-150",
+        "group",
+        className,
+      )}
+      {...props}
+    >
       <span
         className={cn(
-          labelTextStyles(),
+          "text-sm",
+          "font-bold",
+          "text-black",
           "transition-opacity duration-150 group-active:opacity-60",
         )}
       >
@@ -56,7 +31,10 @@ export function Label({ label, value, className, ...props }: LabelProps) {
 
       <span
         className={cn(
-          valueTextStyles(),
+          "font-normal text-sm",
+          "text-dark-gray",
+          "flex items-center gap-2",
+          "before:text-base/tight before:text-dark-gray before:content-['•']",
           "transition-opacity duration-150 group-hover:opacity-80 group-active:opacity-60",
         )}
       >
