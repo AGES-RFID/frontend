b4ef49cae657f4670c034c44d4b90edd4e861967
Arthur <arthurpvschultz18@gmail.com>
2026-04-16 18:23:46 -0300

    fix: format fix

--- src/features/dashboard/components/dashboardCard.tsx
+++ src/features/dashboard/components/dashboardCard.tsx
@@ -2,35 +2,41 @@ import { cva } from "class-variance-authority";
 import type { VariantProps } from "class-variance-authority";
 
 const metricCard = cva(
-    "bg-white rounded-md drop-shadow-lg p-6 flex flex-col gap-2",
-    { 
-        variants: {
-           size: {
-              sm: "p-4",
-              md: "p-6",
-              lg: "p-8",
-         },
-     },
-     defaultVariants: {
-        size: "md",
-     },
-}
-)
+  "bg-white rounded-md drop-shadow-lg p-6 flex flex-col gap-2",
+  {
+    variants: {
+      size: {
+        sm: "p-4",
+        md: "p-6",
+        lg: "p-8",
+      },
+    },
+    defaultVariants: {
+      size: "md",
+    },
+  },
+);
 interface MetricCardProps extends VariantProps<typeof metricCard> {
-    topLabel: string;
-    bottomLabel: string;
-    className?: string;
-    
+  topLabel: string;
+  bottomLabel: string;
+  className?: string;
 }
 
-export function MetricCard({ topLabel, bottomLabel, size, className}: MetricCardProps) {
-    if (!topLabel || !bottomLabel){
-        throw new Error("topLabel e bottomLabel são obrigatórios e não podem ser vazios ")
-    }
-    return (
-        <div className={metricCard({size, className })}>
-            <span className="text-sm text-gray-500 font-medium">{topLabel}</span>
-            <span className="text-4xl font-bold text-gray-900">{bottomLabel} </span>
-        </div>
-    )
+export function MetricCard({
+  topLabel,
+  bottomLabel,
+  size,
+  className,
+}: MetricCardProps) {
+  if (!topLabel || !bottomLabel) {
+    throw new Error(
+      "topLabel e bottomLabel são obrigatórios e não podem ser vazios ",
+    );
+  }
+  return (
+    <div className={metricCard({ size, className })}>
+      <span className="font-medium text-gray-500 text-sm">{topLabel}</span>
+      <span className="font-bold text-4xl text-gray-900">{bottomLabel} </span>
+    </div>
+  );
 }
