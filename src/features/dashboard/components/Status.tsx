5d14988890aa0e4ae4648d8fcd23b028638f1181
Isadora <isadoraspode1@gmail.com>
2026-04-20 21:00:34 -0300

    feat: adiciona componente de status

---
+++ src/features/dashboard/components/Status.tsx
@@ -0,0 +1,44 @@
+
+import React from 'react';
+
+type Status = "ativa" | "livre" | "inativa";
+
+type StatusProps = {
+    status: Status;
+}
+
+const statusConfig: Record<Status, {label: string, className: string, dot: string}> = {
+    ativa: {
+        label: "Ativa",
+        className: "bg-light-green text-dark-gray",
+        dot: "bg-green",
+    },
+    livre: {
+        label: "Livre",
+        className: "bg-yellow text-dark-gray",
+        dot: "bg-dark-orange",
+    },
+    inativa: {
+        label: "Inativa",
+        className: "bg-light-red text-dark-gray",
+        dot: "bg-red",
+    },
+}
+
+export function StatusBadge({status}: StatusProps){
+    const config = statusConfig[status];
+
+    return(
+        <div
+           className={`flex flex-shrink-0 items-center gap-2 px-3 py-1 rounded-full text-sm font-medium ${config.className}`}
+        >
+            <span
+             className={`w-2 h-2 rounded-full ${config.dot}`}
+            />
+            <span>
+                {config.label}
+            </span>
+        </div>
+
+    );
+}
