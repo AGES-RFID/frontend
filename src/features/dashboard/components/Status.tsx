aab29261a03959d003dbe5a46bcacebadcedba60
Isadora <isadoraspode1@gmail.com>
2026-04-20 21:24:02 -0300

    fix: add justify-center and format

--- src/features/dashboard/components/Status.tsx
+++ src/features/dashboard/components/Status.tsx
@@ -1,43 +1,39 @@
-
-
 type Status = "ativa" | "livre" | "inativa";
 
 type StatusProps = {
-    status: Status;
-}
+  status: Status;
+};
 
-const statusConfig: Record<Status, {label: string, className: string, dot: string}> = {
-    ativa: {
-        label: "Ativa",
-        className: "bg-light-green text-dark-gray",
-        dot: "bg-green",
-    },
-    livre: {
-        label: "Livre",
-        className: "bg-yellow text-dark-gray",
-        dot: "bg-dark-orange",
-    },
-    inativa: {
-        label: "Inativa",
-        className: "bg-light-red text-dark-gray",
-        dot: "bg-red",
-    },
-}
+const statusConfig: Record<
+  Status,
+  { label: string; className: string; dot: string }
+> = {
+  ativa: {
+    label: "Ativa",
+    className: "bg-light-green text-dark-gray",
+    dot: "bg-green",
+  },
+  livre: {
+    label: "Livre",
+    className: "bg-yellow text-dark-gray",
+    dot: "bg-dark-orange",
+  },
+  inativa: {
+    label: "Inativa",
+    className: "bg-light-red text-dark-gray",
+    dot: "bg-red",
+  },
+};
 
-export function StatusBadge({status}: StatusProps){
-    const config = statusConfig[status];
+export function StatusBadge({ status }: StatusProps) {
+  const config = statusConfig[status];
 
-    return(
-        <div
-           className={`flex flex-shrink-0 items-center gap-2 px-3 py-1 rounded-full text-sm font-medium ${config.className}`}
-        >
-            <span
-             className={`w-3 h-3 rounded-full ${config.dot}`}
-            />
-            <span>
-                {config.label}
-            </span>
-        </div>
-
-    );
+  return (
+    <div
+      className={`flex w-24 flex-shrink-0 items-center justify-center gap-2 rounded-full px-3 py-1 font-medium text-sm ${config.className}`}
+    >
+      <span className={`h-3 w-3 rounded-full ${config.dot}`} />
+      <span>{config.label}</span>
+    </div>
+  );
 }
