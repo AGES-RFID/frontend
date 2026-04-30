fb25e91e4206c62541fb810f406d791c6c77dd7f
Bruno Panizzi de Quadros <brunopanizziq@gmail.com>
2026-04-29 22:52:28 -0300

    fix: restore vehicleCard

--- src/components/vehicleCard/vehicleCard.tsx
+++ src/components/vehicleCard/vehicleCard.tsx
@@ -5,7 +5,7 @@ import { CarIcon } from "./carIcon";
 type VehicleCardProps = {
   size?: "lg" | "sm";
   hasDelete?: boolean;
-  licensePlate?: string;
+  licensePlate: string;
   onClick?: () => void;
   onDelete?: () => void;
 };
@@ -20,31 +20,35 @@ export function VehicleCard({
   const isLarge = size === "lg";
 
   return (
-    <div className="relative">
-      <button
-        type="button"
-        onClick={onClick}
+    <button
+      type="button"
+      onClick={onClick}
+      className={cn(
+        "relative flex cursor-pointer flex-col items-center justify-center border-2 border-dark-blue font-bold",
+        isLarge ? "size-36 rounded-4xl" : "size-18.5 rounded-2xl",
+      )}
+    >
+      {/*ícone carro*/}
+      <CarIcon size={isLarge ? 64 : 28} className="text-dark-blue" />
+
+      {/*placa*/}
+      <span
         className={cn(
-          "flex cursor-pointer flex-col items-center justify-center border-2 border-dark-blue font-bold",
-          isLarge ? "size-36 rounded-4xl" : "size-18.5 rounded-2xl",
+          "text-black",
+          isLarge ? "mt-2 text-[28px]" : "mt-1 text-sm",
         )}
       >
-        <CarIcon size={isLarge ? 64 : 28} className="text-dark-blue" />
+        {licensePlate}
+      </span>
 
-        <span
-          className={cn(
-            "text-black",
-            isLarge ? "mt-2 text-[28px]" : "mt-1 text-sm",
-          )}
-        >
-          {licensePlate || ""}
-        </span>
-      </button>
-
+      {/*delete*/}
       {hasDelete && (
         <button
           type="button"
-          onClick={onDelete}
+          onClick={(e) => {
+            e.stopPropagation();
+            onDelete?.();
+          }}
           className={cn(
             "absolute cursor-pointer text-black transition-colors hover:text-red",
             isLarge ? "top-2 right-2" : "top-1 right-1",
@@ -53,6 +57,6 @@ export function VehicleCard({
           <Trash2 size={isLarge ? 18 : 14} />
         </button>
       )}
-    </div>
+    </button>
   );
 }
